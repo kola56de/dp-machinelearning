364ea78f772311e3a5f6bf3325f2a58b6e44550c
Engr. Dr. Kolade Julius Olonisakin, FNSE <kolaolonisakin@gmail.com>
2025-09-23 13:29:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Machine Learning App')
 
-st.write('Hello world!')
+st.info('This is a machine learning app')
+st.info('This app builds a machine learing model')
+
