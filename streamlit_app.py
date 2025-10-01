d2d736f94e94ac49b21d8bdf1839887b6cc0c949
Engr. Dr. Kolade Julius Olonisakin, FNSE <kolaolonisakin@gmail.com>
2025-10-01 13:37:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ with st.expander ('Data'):
   df
   
   st.write('**X**')
-  X = df.drop('species', axis = 1)
-  X
+  X_raw = df.drop('species', axis = 1)
+  X_raw
 
   st.write('**Y**')
-  Y = df.species
-  Y
+  Y_raw = df.species
+  Y_raw
 
 with st.expander ('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color = 'species')
@@ -40,13 +40,20 @@ with st.sidebar:
           'sex': gender
          }
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
   
-  # Encode
-  encode = ['island', 'sex']
-  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-  input_row = df_penguins[:1]
+# Encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
 
+# Encode Y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2
+                }
+def target_encode(val):
+  return target_mapper[val]
 with st.expander('Input Features'):
   st.write('**Input Penguin**')
   input_df
