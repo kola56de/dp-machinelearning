4cbbdc0b5db34b72ac3d462cb89e57ade72541d8
Engr. Dr. Kolade Julius Olonisakin, FNSE <kolaolonisakin@gmail.com>
2025-10-01 11:39:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learing model')
-
+df = read_csv('https://raw.githubusercontent.com/kola56de/dp-machinelearning/refs/heads/master/penguins_cleaned.csv')
+df
