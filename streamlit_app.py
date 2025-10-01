3b13674be3890140afc1f5d246e673d952898d7f
Engr. Dr. Kolade Julius Olonisakin, FNSE <kolaolonisakin@gmail.com>
2025-10-01 12:18:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ with st.expander ('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color = 'species')
 
 # Data Preparation
-with st.sidebar:
+with st.sidebar
   
   
